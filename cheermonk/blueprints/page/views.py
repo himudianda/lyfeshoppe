47850c10d79926cf2d8c4f54237b2e40fdf71f96
Harshit Imudianda <harshit.imudianda@gmail.com>
2015-11-11 11:23:51 -0800

    Towards building templates for frontend pages

--- cheermonk/blueprints/page/views.py
+++ cheermonk/blueprints/page/views.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, render_template
 
-page = Blueprint('page', __name__)
+page = Blueprint('page', __name__, template_folder='templates')
 
 
-# curl -i -X GET http://localhost:8000
+# curl -i -X GET http://localhost:5000
 @page.route('/')
 def home():
-    return jsonify({"Success": True})
+    return render_template('page/home.jinja2')
