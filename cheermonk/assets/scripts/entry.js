5d1109b359eedff7354a3d14ba411dcfbc346de0
Harshit Imudianda <harshit.imudianda@gmail.com>
2015-11-13 12:10:50 -0800

    Added styles, fonts, images scripts to make the webpage easy to navigate & better looking.

--- cheermonk/assets/scripts/entry.js
+++ cheermonk/assets/scripts/entry.js
@@ -1,3 +1,19 @@
-(function() {
+var moment = require('moment'); // included thru node_modules & nginx could serve it directly.
 
-})();
+$(document).ready(function () {
+
+    $('.from-now').each(function (i, e) {
+        (function updateTime() {
+            var time = moment($(e).data('datetime'));
+            $(e).text(time.fromNow());
+            $(e).attr('title', time.format('MMMM Do YYYY, h:mm:ss a Z'));
+            setTimeout(updateTime, 1000);
+        })();
+    });
+
+    $('.short-date').each(function (i, e) {
+        var time = moment($(e).data('datetime'));
+        $(e).text(time.format('MMM Do YYYY'));
+        $(e).attr('title', time.format('MMMM Do YYYY, h:mm:ss a Z'));
+    });
+});
