be1f6a7776f74da6a428e01d954d0e168d1faacb
Harshit Imudianda <harshit.imudianda@gmail.com>
2015-12-03 09:38:30 -0800

    Redirect to dashboard page for some cases which were broken

--- cheermonk/blueprints/user/decorators.py
+++ cheermonk/blueprints/user/decorators.py
@@ -1,16 +1,14 @@
 from functools import wraps
 
-from flask import flash, redirect
+from flask import flash, redirect, url_for
 from flask_babel import gettext as _
 from flask_login import current_user
 
 
-def anonymous_required(url='/settings'):
+def anonymous_required():
     """
     Redirect a user to a specified location if they are already signed in.
 
-    :param url: URL to be redirected to if invalid
-    :type url: str
     :return: Function
     """
 
@@ -18,7 +16,7 @@ def anonymous_required(url='/settings'):
         @wraps(f)
         def decorated_function(*args, **kwargs):
             if current_user.is_authenticated():
-                return redirect(url)
+                return redirect(url_for('page.dashboard'))
 
             return f(*args, **kwargs)
 
@@ -40,7 +38,7 @@ def role_required(*roles):
         def decorated_function(*args, **kwargs):
             if current_user.role not in roles:
                 flash(_('You do not have permission to do that.'), 'error')
-                return redirect('/')
+                return redirect(url_for('page.dashboard'))
 
             return f(*args, **kwargs)
 
