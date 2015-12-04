435003256c85d582edaec96e09abe4fc902b3645
Harshit Imudianda <harshit.imudianda@gmail.com>
2015-12-04 14:45:25 -0800

    Products & Inventory models added

--- cheermonk/blueprints/business/models/business.py
+++ cheermonk/blueprints/business/models/business.py
@@ -5,6 +5,43 @@ from cheermonk.lib.util_sqlalchemy import ResourceMixin
 from cheermonk.extensions import db
 
 
+class Inventory(ResourceMixin, db.Model):
+    __tablename__ = 'inventory'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    # Relationships.
+    product_id = db.Column(db.Integer, db.ForeignKey(
+                        'products.id', onupdate='CASCADE', ondelete='CASCADE'
+                    ), index=True, nullable=False)
+    price = db.Column(db.Integer, nullable=False, default=0)
+
+    # Example: Massage has count 1
+    # Example: Yoga classes & sports training can how count >= 1
+    count = db.Column(db.Integer, nullable=False, default=0)
+
+
+class Product(ResourceMixin, db.Model):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    title = db.Column(db.String(128), nullable=False, server_default='')
+    description = db.Column(db.Text())
+    price = db.Column(db.Integer, nullable=False, default=0)
+
+    inventory = db.relationship(Inventory, backref='products', passive_deletes=True)
+
+    # Relationships.
+    business_id = db.Column(db.Integer, db.ForeignKey(
+                        'businesses.id', onupdate='CASCADE', ondelete='CASCADE'
+                    ), index=True, nullable=False)
+
+    def __init__(self, **kwargs):
+        # Call Flask-SQLAlchemy's constructor.
+        super(Product, self).__init__(**kwargs)
+
+
 class Business(ResourceMixin, db.Model):
     __tablename__ = 'businesses'
     id = db.Column(db.Integer, primary_key=True)
@@ -25,6 +62,8 @@ class Business(ResourceMixin, db.Model):
     name = db.Column(db.String(255))
     email = db.Column(db.String(255), unique=True, index=True, nullable=False, server_default='')
     type = db.Column(db.Enum(*TYPE, name='business_types'), index=True, nullable=False, server_default='massage')
+
+    products = db.relationship(Product, backref='businesses', passive_deletes=True)
 
     def __init__(self, **kwargs):
         # Call Flask-SQLAlchemy's constructor.
