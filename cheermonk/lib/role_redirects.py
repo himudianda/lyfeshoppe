7d4c8c0c6da2b1a466925c9a12c70c4ab2d2f850
Harshit Imudianda <harshit.imudianda@gmail.com>
2015-12-04 11:48:07 -0800

    More work to ensure backend/ module can be used instead of dashboard/ moduke

--- cheermonk/lib/role_redirects.py
+++ cheermonk/lib/role_redirects.py
@@ -6,6 +6,6 @@ def get_dashboard_url():
     if current_user.role == "admin":
         return url_for('admin.dashboard')
     elif current_user.role == "member":
-        return url_for('dashboard.index')
+        return url_for('backend.dashboard')
     else:
         return url_for('page.home')
