6a620bbd8f08617059d31d6ede060e963c5e58f4
Harshit Imudianda <harshit.imudianda@gmail.com>
2016-01-06 17:53:54 -0800

    Display times in Pacific time

--- lyfeshoppe/blueprints/backend/template_processors.py
+++ lyfeshoppe/blueprints/backend/template_processors.py
@@ -1,9 +1,11 @@
 import babel
+from babel.dates import get_timezone
 
 
 def format_datetime(value, format='medium'):
+    pacific = get_timezone('US/Pacific')
     if format == 'full':
         format = "EEEE, d. MMMM y 'at' h:m a"
     elif format == 'medium':
         format = "MM/dd/yy h:mm a"  # look at babel time documentation for formatting
-    return babel.dates.format_datetime(value, format)
+    return babel.dates.format_datetime(value, format, tzinfo=pacific)
