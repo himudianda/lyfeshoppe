28d01131e52f49a8cdc5608eea0509f88f9a996c
Harshit Imudianda <harshit.imudianda@gmail.com>
2016-01-07 16:53:24 -0800

    Also create Address for User when User is created

--- lyfeshoppe/blueprints/user/models.py
+++ lyfeshoppe/blueprints/user/models.py
@@ -102,6 +102,7 @@ class User(UserMixin, ResourceMixin, db.Model):
                 form.populate_obj(user.address)
             else:
                 user = cls(**params)
+                user.address = Address(**params)
 
             # Generate a random password
             pwd_size = 8
