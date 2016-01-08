aa5d5c6a377103b77cee72e59151d8a6bad0f1dc
Harshit Imudianda <harshit.imudianda@gmail.com>
2016-01-07 23:05:35 -0800

    Added employees for businesses

--- cli/commands/cmd_add.py
+++ cli/commands/cmd_add.py
@@ -37,8 +37,9 @@ db.app = app
 
 NUM_OF_FAKE_USERS = 50
 NUM_OF_FAKE_ISSUES = 50
-NUM_OF_FAKE_COUPONS = 5
 NUM_OF_FAKE_BUSINESSES = 200
+MAX_EMPLOYEES_PER_BUSINESS = 10
+MAX_PRODUCTS_PER_BUSINESS = 25
 NUM_OF_FAKE_RESERVATIONS = 10000
 
 
@@ -170,6 +171,60 @@ def issues():
 
 
 @click.command()
+def employees():
+    """
+    Create random employees.
+    """
+    data = []
+    users = db.session.query(User).all()
+    businesses = db.session.query(Business).all()
+
+    for business in businesses:
+        num_of_employees = random.randint(0, MAX_EMPLOYEES_PER_BUSINESS)
+
+        users_list = list(users)
+        to_be_employees = random.sample(users_list, num_of_employees)
+
+        for user in to_be_employees:
+            params = {
+                'business_id': business.id,
+                'user_id': user.id,
+                'about': fake.paragraph(nb_sentences=6, variable_nb_sentences=True),
+                'active': '1'
+            }
+
+            data.append(params)
+
+    return _bulk_insert(Employee, data, 'employees')
+
+
+@click.command()
+def products():
+    """
+    Create random products.
+    """
+    data = []
+    businesses = db.session.query(Business).all()
+
+    for business in businesses:
+        num_of_products = random.randint(0, MAX_PRODUCTS_PER_BUSINESS)
+        for i in range(0, num_of_products):
+            params = {
+                'name': fake.text(max_nb_chars=48),
+                'category': random.choice(['Massage', 'Makeup', 'Nail', 'Haircut', 'Waxing']),
+                'description': fake.paragraph(nb_sentences=6, variable_nb_sentences=True),
+                'capacity': random.randint(1, 100),
+                'price_cents': random.randint(100, 100000),
+                'duration_mins': random.randint(10, 180),
+                'business_id': business.id
+            }
+
+            data.append(params)
+
+    return _bulk_insert(Product, data, 'products')
+
+
+@click.command()
 def customers():
     """
     Create random customers.
@@ -187,80 +242,6 @@ def customers():
         data.append(params)
 
     return _bulk_insert(Customer, data, 'customers')
-
-
-@click.command()
-def products():
-    """
-    Create random products.
-    """
-    data = []
-    businesses = db.session.query(Business).all()
-
-    for business in businesses:
-        for i in range(0, random.randint(1, 10)):
-            params = {
-                'name': fake.text(max_nb_chars=48),
-                'category': random.choice(['Massage', 'Makeup', 'Nail', 'Haircut', 'Waxing']),
-                'description': fake.paragraph(nb_sentences=6, variable_nb_sentences=True),
-                'capacity': random.randint(1, 100),
-                'price_cents': random.randint(100, 100000),
-                'duration_mins': random.randint(10, 180),
-                'business_id': business.id
-            }
-
-            data.append(params)
-
-    return _bulk_insert(Product, data, 'products')
-
-
-@click.command()
-def employees():
-    """
-    Create random employees.
-    """
-    data = []
-    businesses = db.session.query(Business).all()
-
-    for business in businesses:
-        employees_list = []
-        for i in range(0, random.randint(12, 20)):
-
-            if len(employees_list):
-                admin_employee = random.choice(
-                    db.session.query(User).filter(~User.id.in_(employees_list)).all()
-                )
-            else:
-                admin_employee = random.choice(
-                    db.session.query(User).all()
-                )
-            employees_list.append(admin_employee.id)
-            params = {
-                'role': 'admin',
-                'business_id': business.id,
-                'user_id': admin_employee.id,
-                'products': Product.query.filter(Product.business_id == business.id).all(),
-                'about': fake.paragraph(nb_sentences=6, variable_nb_sentences=True),
-                'active': '1'
-            }
-            data.append(params)
-
-            # Ensure that the member employee isnt the same as the admin employee
-            member_employee = random.choice(
-                db.session.query(User).filter(~User.id.in_(employees_list)).all()
-            )
-            employees_list.append(member_employee.id)
-            params = {
-                'role': 'member',
-                'business_id': business.id,
-                'user_id': member_employee.id,
-                'products': Product.query.filter(Product.business_id == business.id).all(),
-                'about': fake.paragraph(nb_sentences=6, variable_nb_sentences=True),
-                'active': '1'
-            }
-            data.append(params)
-
-    return _bulk_insert(Employee, data, 'employees')
 
 
 @click.command()
@@ -327,10 +308,6 @@ def all(ctx):
     ctx.invoke(issues)
     ctx.invoke(businesses)
     ctx.invoke(employees)
-    ctx.invoke(products)
-    ctx.invoke(employee_product_relations)
-    ctx.invoke(customers)
-    ctx.invoke(reservations)
     return None
 
 
@@ -338,8 +315,4 @@ cli.add_command(users)
 cli.add_command(issues)
 cli.add_command(businesses)
 cli.add_command(employees)
-cli.add_command(products)
-cli.add_command(employee_product_relations)
-cli.add_command(customers)
-cli.add_command(reservations)
 cli.add_command(all)
--- lyfeshoppe/blueprints/common/models.py
+++ lyfeshoppe/blueprints/common/models.py
@@ -49,8 +49,8 @@ class Address(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     street = db.Column(db.String(256))
-    city = db.Column(db.String(50))
-    state = db.Column(db.String(50))
+    city = db.Column(db.String(100))
+    state = db.Column(db.String(100))
     zipcode = db.Column(db.String(20))
-    district = db.Column(db.String(50))  # or county name
-    country = db.Column(db.String(50))
+    district = db.Column(db.String(100))  # or county name
+    country = db.Column(db.String(100))
