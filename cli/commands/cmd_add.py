0f6917faee7c1718d1d455f9e952595a8d71682c
Harshit Imudianda <harshit.imudianda@gmail.com>
2016-01-07 21:14:41 -0800

    Dont generate unwanted fake data - occupancies, availabilities, coupons & invoices

--- cli/commands/cmd_add.py
+++ cli/commands/cmd_add.py
@@ -8,10 +8,7 @@ from lyfeshoppe.app import create_app
 from lyfeshoppe.extensions import db
 from lyfeshoppe.blueprints.issue.models import Issue
 from lyfeshoppe.blueprints.user.models import User
-from lyfeshoppe.blueprints.common.models import Address, Availability, Occupancy
-from lyfeshoppe.blueprints.billing.models.invoice import Invoice
-from lyfeshoppe.blueprints.billing.models.coupon import Coupon
-from lyfeshoppe.blueprints.billing.gateways.stripecom import Coupon as PaymentCoupon
+from lyfeshoppe.blueprints.common.models import Address
 from lyfeshoppe.blueprints.business.models.business import Business, Employee, Product, Customer, Reservation
 
 SEED_ADMIN_EMAIL = None
@@ -164,119 +161,6 @@ def issues():
 
 
 @click.command()
-def coupons():
-    """
-    Create random coupons.
-    """
-    data = []
-
-    for i in range(0, NUM_OF_FAKE_COUPONS):
-        random_pct = random.random()
-        duration = random.choice(Coupon.DURATION.keys())
-
-        # Create a fake unix timestamp in the future.
-        redeem_by = fake.date_time_between(start_date='now',
-                                           end_date='+1y').strftime('%s')
-
-        # Bulk inserts need the same columns, so let's setup a few nulls.
-        params = {
-            'code': Coupon.random_coupon_code(),
-            'duration': duration,
-            'percent_off': None,
-            'amount_off': None,
-            'currency': None,
-            'redeem_by': None,
-            'max_redemptions': None,
-            'duration_in_months': None,
-        }
-
-        if random_pct >= 0.5:
-            params['percent_off'] = random.randint(1, 100)
-            params['max_redemptions'] = random.randint(15, 50)
-        else:
-            params['amount_off'] = random.randint(1, 1337)
-            params['currency'] = 'usd'
-
-        if random_pct >= 0.75:
-            params['redeem_by'] = redeem_by
-
-        if duration == 'repeating':
-            duration_in_months = random.randint(1, 12)
-            params['duration_in_months'] = duration_in_months
-
-        PaymentCoupon.create(**params)
-
-        # Our database requires a Date object, not a unix timestamp.
-        if redeem_by:
-            params['redeem_by'] = datetime.utcfromtimestamp(float(redeem_by)) \
-                .strftime('%Y-%m-%dT%H:%M:%S Z')
-
-        if 'id' in params:
-            params['code'] = params.get('id')
-            del params['id']
-
-        data.append(params)
-
-    return _bulk_insert(Coupon, data, 'coupons')
-
-
-@click.command()
-def invoices():
-    """
-    Create random invoices.
-    """
-    data = []
-
-    users = db.session.query(User).all()
-
-    for user in users:
-        for i in range(0, random.randint(1, 12)):
-            # Create a fake unix timestamp in the future.
-            created_on = fake.date_time_between(
-                start_date='-1y', end_date='now').strftime('%s')
-            period_start_on = fake.date_time_between(
-                start_date='now', end_date='+1y').strftime('%s')
-            period_end_on = fake.date_time_between(
-                start_date=period_start_on, end_date='+14d').strftime('%s')
-            exp_date = fake.date_time_between(
-                start_date='now', end_date='+2y').strftime('%s')
-
-            created_on = datetime.utcfromtimestamp(
-                float(created_on)).strftime('%Y-%m-%dT%H:%M:%S Z')
-            period_start_on = datetime.utcfromtimestamp(
-                float(period_start_on)).strftime('%Y-%m-%d')
-            period_end_on = datetime.utcfromtimestamp(
-                float(period_end_on)).strftime('%Y-%m-%d')
-            exp_date = datetime.utcfromtimestamp(
-                float(exp_date)).strftime('%Y-%m-%d')
-
-            plans = ['BRONZE', 'GOLD', 'PLATINUM']
-            cards = ['Visa', 'Mastercard', 'AMEX',
-                     'J.C.B', "Diner's Club"]
-
-            params = {
-                'created_on': created_on,
-                'updated_on': created_on,
-                'user_id': user.id,
-                'receipt_number': fake.md5(),
-                'description': '{0} MONTHLY'.format(random.choice(plans)),
-                'period_start_on': period_start_on,
-                'period_end_on': period_end_on,
-                'currency': 'usd',
-                'tax': random.random() * 100,
-                'tax_percent': random.random() * 10,
-                'total': random.random() * 1000,
-                'brand': random.choice(cards),
-                'last4': random.randint(1000, 9000),
-                'exp_date': exp_date
-            }
-
-            data.append(params)
-
-    return _bulk_insert(Invoice, data, 'invoices')
-
-
-@click.command()
 def businesses():
     """
     Create random businesses.
@@ -309,68 +193,6 @@ def businesses():
         data.append(params)
 
     return _bulk_insert(Business, data, 'businesses')
-
-
-@click.command()
-def occupancies():
-    """
-    Create random business occupanices.
-    """
-    data = []
-
-    users = db.session.query(User).all()
-    for user in users:
-        for i in range(0, random.randint(1, 12)):
-
-            # Create a fake unix timestamp in the future.
-            start_time = fake.date_time_between(
-                start_date='now', end_date='+1d').strftime('%s')
-            end_time = fake.date_time_between(
-                start_date=start_time, end_date='+2d').strftime('%s')
-
-            start_time = datetime.utcfromtimestamp(
-                float(start_time)).strftime('%Y-%m-%d %H:%M:%S')
-            end_time = datetime.utcfromtimestamp(
-                float(end_time)).strftime('%Y-%m-%d %H:%M:%S')
-
-            params = {
-                'type': 'user',
-                'start_time': start_time,
-                'end_time': end_time,
-                'user_id': user.id,
-                'business_id': None,
-                'active': '1'
-            }
-
-            data.append(params)
-
-    businesses = db.session.query(Business).all()
-    for business in businesses:
-        for i in range(0, random.randint(1, 12)):
-
-            # Create a fake unix timestamp in the future.
-            start_time = fake.date_time_between(
-                start_date='now', end_date='+1d').strftime('%s')
-            end_time = fake.date_time_between(
-                start_date=start_time, end_date='+2d').strftime('%s')
-
-            start_time = datetime.utcfromtimestamp(
-                float(start_time)).strftime('%Y-%m-%d %H:%M:%S')
-            end_time = datetime.utcfromtimestamp(
-                float(end_time)).strftime('%Y-%m-%d %H:%M:%S')
-
-            params = {
-                'type': 'business',
-                'start_time': start_time,
-                'end_time': end_time,
-                'user_id': None,
-                'business_id': business.id,
-                'active': '1'
-            }
-
-            data.append(params)
-
-    return _bulk_insert(Occupancy, data, 'occupancies')
 
 
 @click.command()
@@ -480,41 +302,6 @@ def employee_product_relations():
 
 
 @click.command()
-def availabilities():
-    """
-    Create random product availabilities.
-    """
-    data = []
-
-    products = db.session.query(Product).all()
-    for product in products:
-        for i in range(0, random.randint(1, 12)):
-
-            # Create a fake unix timestamp in the future.
-            start_time = fake.date_time_between(
-                start_date='now', end_date='+1d').strftime('%s')
-            end_time = fake.date_time_between(
-                start_date=start_time, end_date='+2d').strftime('%s')
-
-            start_time = datetime.utcfromtimestamp(
-                float(start_time)).strftime('%Y-%m-%d %H:%M:%S')
-            end_time = datetime.utcfromtimestamp(
-                float(end_time)).strftime('%Y-%m-%d %H:%M:%S')
-
-            params = {
-                'type': 'product',
-                'start_time': start_time,
-                'end_time': end_time,
-                'product_id': product.id,
-                'active': '1'
-            }
-
-            data.append(params)
-
-    return _bulk_insert(Availability, data, 'availabilities')
-
-
-@click.command()
 def reservations():
     """
     Create random reservations.
@@ -565,14 +352,10 @@ def all(ctx):
     ctx.invoke(addresses)
     ctx.invoke(users)
     ctx.invoke(issues)
-    #ctx.invoke(coupons)
-    ctx.invoke(invoices)
     ctx.invoke(businesses)
-    ctx.invoke(occupancies)
     ctx.invoke(employees)
     ctx.invoke(products)
     ctx.invoke(employee_product_relations)
-    ctx.invoke(availabilities)
     ctx.invoke(customers)
     ctx.invoke(reservations)
     return None
@@ -581,14 +364,10 @@ def all(ctx):
 cli.add_command(addresses)
 cli.add_command(users)
 cli.add_command(issues)
-#cli.add_command(coupons)
-cli.add_command(invoices)
 cli.add_command(businesses)
-cli.add_command(occupancies)
 cli.add_command(employees)
 cli.add_command(products)
 cli.add_command(employee_product_relations)
-cli.add_command(availabilities)
 cli.add_command(customers)
 cli.add_command(reservations)
 cli.add_command(all)
